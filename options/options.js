0cfaf586ff59546d651ba44b582db4203e6a6ccc
Crowdbotics <engineering@crowdbotics.com>
2023-04-11 00:17:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fragrant-cherry-dev-75765",
+  url: "https://fragrant-cherry-dev-75765.botics.co",
+  api: "https://fragrant-cherry-dev-75765.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
